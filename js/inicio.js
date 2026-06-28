b873297ef71bae2f3ced6f0f760bc594b81cc36e
elprofemendieta-creator <elprofemendieta@gmail.com>
2026-06-27 20:10:53 -0600

    Update inicio.js

--- js/inicio.js
+++ js/inicio.js
@@ -740,6 +740,9 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('🎠 Carrusel Premium inicializado correctamente');
 });
 
+
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('Service Worker registrado', reg))
+    .catch(err => console.error('Error al registrar SW', err));
 }
